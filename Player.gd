ef9bed0f3bc5a805c7b6d3ddd5c4d2d0e93e9da7
David Konsumer <konsumer@jetboystudio.com>
2020-04-15 15:13:02 -0700

    add more real-game features

---
+++ Player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+export var speed = 200
+var velocity = Vector2()
+var direction = "down"
+
+func _ready():
+	$anim.play("wait down")
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_left"):
+		direction = "left"
+		velocity.x = -speed
+	elif Input.is_action_pressed("ui_right"):
+		direction = "right"
+		velocity.x = speed
+	else:
+		velocity.x = 0
+		
+	if Input.is_action_pressed("ui_up"):
+		direction = "up"
+		velocity.y = -speed
+	elif Input.is_action_pressed("ui_down"):
+		direction = "down"
+		velocity.y = speed
+	else:
+		velocity.y = 0
+	
+	if velocity.x == 0 and velocity.y == 0:
+		$anim.play("wait " + direction)
+	else:
+		$anim.play("walk " + direction)
+		
+	move_and_collide(velocity * delta)
